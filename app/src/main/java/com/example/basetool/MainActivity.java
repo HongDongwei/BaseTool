a37d31ec5c4888f105b6b2cb6a920f7a7d9ee6cf
HongDongwei <15869109684@163.com>
2020-05-18 10:51:39 +0800

    修改

--- app/src/main/java/com/example/basetool/MainActivity.java
+++ app/src/main/java/com/example/basetool/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.basetool;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
